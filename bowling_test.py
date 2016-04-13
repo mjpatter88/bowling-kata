05f958df5c81b86718b8334c41f7e63dde967429
Michael Patterson <mjpatter88@gmail.com>
2016-04-12 22:59:16 -0500

    RED: Test that all gutter balls results in a score of 0.

--- bowling_test.py
+++ bowling_test.py
@@ -1,6 +1,9 @@
 import unittest
+import bowling
 
 class BowlingTest(unittest.TestCase):
 
-    def test_first(self):
-        self.asserEquals(True, False, "First Test")
+    def test_score_returns_zero_for_all_gutter_balls(self):
+        balls = [0] * 10
+        score = bowling.score(balls)
+        self.asserEquals(score, 0, "All gutter balls should result in a score of 0.")
