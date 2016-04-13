282fbd748cb20e1658722520d016d5f4194ca5b1
Michael Patterson <mjpatter88@gmail.com>
2016-04-13 08:24:02 -0500

    RED: Test that a frame with five and three results in a score of 8.

--- bowling_test.py
+++ bowling_test.py
@@ -2,8 +2,14 @@ import unittest
 import bowling
 
 class BowlingTest(unittest.TestCase):
+    def setUp(self):
+        self.balls = [(0,0)] * 10
 
-    def test_score_returns_zero_for_all_gutter_balls(self):
-        balls = [0] * 10
-        score = bowling.score(balls)
+    def test_score__returns_zero_for_all_gutter_balls(self):
+        score = bowling.score(self.balls)
         self.assertEquals(score, 0, "All gutter balls should result in a score of 0.")
+
+    def test_score__each_no_spare_or_strike_frame_adds_pin_total_to_score(self):
+        self.balls[0] = (5, 3)
+        score = bowling.score(self.balls)
+        self.assertEquals(score, 8, "Five and three should result in a score of 8.")
