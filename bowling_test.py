6a641520c9dbe1998b3462733a4ffa7523b4ae29
Michael Patterson <mjpatter88@gmail.com>
2016-04-14 09:44:18 -0500

    RED: Test that a spare frame adds the next throw into its total.

--- bowling_test.py
+++ bowling_test.py
@@ -19,3 +19,9 @@ class BowlingTest(unittest.TestCase):
         self.frames[1] = (8, 1)
         score = bowling.score(self.frames)
         self.assertEquals(score, 17, "Two frames should be added together.")
+
+    def test_score__adds_the_first_throw_from_the_next_frame_to_the_current_frame_total_if_spare(self):
+        self.frames[0] = (9, 1)
+        self.frames[1] = (5, 3)
+        score = bowling.score(self.frames)
+        self.assertEquals(score, 23, "Spare frame should add next throw to total.")
